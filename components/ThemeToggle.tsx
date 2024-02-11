1b1d5da7e67af15766ae515b669a5aced3beb3a5
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-02-11 11:43:11 -0500

    WIP: primary features

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -6,7 +6,6 @@ import { Switch } from './ui/switch'
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
-  console.log(theme)
   return (
     <div className="flex scale-125 flex-col items-center justify-center gap-2 md:flex-row">
       <Sun className="h-4 w-4 rotate-0 transition-all duration-500 ease-in-out dark:-rotate-90 dark:opacity-0" />
--- components/block/PrimaryFeatureBlock.tsx
+++ components/block/PrimaryFeatureBlock.tsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import GridSectionContainer from '../GridSectionContainer'
 import BentoChildContainer from '../BentoChildContainer'
+import { Shapes, Code, RocketLaunch } from '@phosphor-icons/react/dist/ssr'
 
 const PrimaryFeatureBlock = () => {
   return (
     <GridSectionContainer>
-      <BentoChildContainer className="col-span-4 row-span-2 rounded-2xl">
-        <p className="text-3xl leading-tight text-black">
+      <BentoChildContainer className="col-span-4 row-span-4 rounded-2xl">
+        <p className="text-3xl leading-tight">
           Let me run your website, so you can run your business{' '}
-          <span className="text-stone-700">
+          <span className="text-stone-500">
             – From expert web development and captivating design to seamless user experiences, I've
             got you covered.
           </span>
         </p>
       </BentoChildContainer>
-      <BentoChildContainer className="col-span-2 row-span-2 rounded-2xl">
+      <BentoChildContainer className="col-span-2 row-span-4 rounded-2xl">
         <h2 className="text-4xl font-bold leading-12 tracking-tight">
           Everything You Need To Establish An Online Presence
         </h2>
       </BentoChildContainer>
-      <BentoChildContainer className="col-span-2 rounded-2xl text-2xl">Design</BentoChildContainer>
-      <BentoChildContainer className="col-span-2 rounded-2xl text-2xl">Develop</BentoChildContainer>
-      <BentoChildContainer className="col-span-2 rounded-2xl text-2xl">Deploy</BentoChildContainer>
+      <BentoChildContainer className="col-span-2 row-span-2 flex items-center gap-x-2 rounded-2xl text-2xl">
+        <Shapes size={32} className="text-stone-500" />
+        Design
+      </BentoChildContainer>
+      <BentoChildContainer className="col-span-2 row-span-2 flex items-center gap-x-2 rounded-2xl text-2xl">
+        <Code size={32} className="text-stone-500" />
+        Develop
+      </BentoChildContainer>
+      <BentoChildContainer className="col-span-2 row-span-2 flex items-center gap-x-2 rounded-2xl text-2xl">
+        <RocketLaunch size={32} className="text-stone-500" />
+        Deploy
+      </BentoChildContainer>
     </GridSectionContainer>
   )
 }
