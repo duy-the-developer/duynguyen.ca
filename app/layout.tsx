800db2928b797fca6d6973f45f3c9ac3fc2224e1
Duy <duythedeveloper@gmail.com>
2025-05-27 09:04:49 -0400

    replace favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,16 +71,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${interTight.variable} ${jetbrainsMono.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-      <link rel="manifest" href="/static/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/svg+xml" href="/static/favicon/favicon.svg" />
+      <link rel="icon" type="image/png" href="/static/favicon-96x96.png" sizes="96x96" />
+      <link rel="manifest" href="/static/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/static/favicon/favicon.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="relative min-h-screen bg-stone-200 text-stone-900 antialiased dark:bg-stone-950 dark:text-stone-200">
+      <body className="relative min-h-screen bg-stone-100 text-stone-900 antialiased dark:bg-stone-950 dark:text-stone-200">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <div>
